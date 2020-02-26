4e96b0b4fd0f7364cff554b328af1c4b309d58f3
Simon Thulbourn <sthulb@users.noreply.github.com>
2020-02-26 20:15:46 +0000

    base region/account on env

--- stack/bin/stack.ts
+++ stack/bin/stack.ts
@@ -3,8 +3,10 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { StackStack } from '../lib/stack-stack';
 
+import * as process from 'process';
+
 const app = new cdk.App();
 new StackStack(app, 'StackStack', {env: {
-  region: 'eu-west-1',
-  account: '409303902518'
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
 }});
